9e2fa34717eb301f919da1827e30fdcb9e4dd17a
xaniles <yufei.xi@yahoo.com>
2020-02-09 17:36:26 +0100

    Initial Commit

---
+++ Flashcard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcard
+//
+//  Created by Selina on 2020/2/9.
+//  Copyright © 2020 Selina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
